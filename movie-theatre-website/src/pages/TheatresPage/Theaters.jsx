1aa54c1b330b682603fbd57f88b34da5e6446b65
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-10-20 12:59:38 +0300

    Map added -> theaterspage

--- movie-theatre-website/src/pages/TheatresPage/Theaters.jsx
+++ movie-theatre-website/src/pages/TheatresPage/Theaters.jsx
@@ -2,12 +2,14 @@ import PageContainer from "../../components/PageContainer/PageContainer";
 import TheatresContainer from "./components/TheatresContainer/TheatresContainer";
 import "./Theatres.scss";
 import AnimatePage from "../../components/AnmatePages/AnimatePage";
+import SimpleMap from "./components/TheatresMap/TheatresMap";
 
 export default function AllTheatresPage(props) {
   return (
     <PageContainer>
       <AnimatePage>
         <TheatresContainer className="container-style" />
+        <SimpleMap />
       </AnimatePage>
     </PageContainer>
   );
