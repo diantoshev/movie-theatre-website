7e8fb1a659c81a3044f0945b96ef00015d3997da
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-10-22 16:03:02 +0300

    Theater halls panel removed, theater details,info, panel, screenings styles improved

--- movie-theatre-website/src/pages/TheatreDetailPage/components/TheaterPanel/TheaterPanel.jsx
+++ movie-theatre-website/src/pages/TheatreDetailPage/components/TheaterPanel/TheaterPanel.jsx
@@ -7,13 +7,13 @@ import TheaterScreenings from "../TheaterScreenings/TheaterScreening";
 export default function TheaterPanel() {
   return (
     <GreyContainer className="theater-big-panel-design">
-      <GoldContainer className="theater-panel-design">
+      {/* <GoldContainer className="theater-panel-design">
         <DarkButton className="btn-hall">Hall 1</DarkButton>
         <DarkButton className="btn-hall">Hall 2</DarkButton>
         <DarkButton className="btn-hall">Hall 3</DarkButton>
         <DarkButton className="btn-hall">Hall 4</DarkButton>
         <DarkButton className="btn-hall">Hall 5</DarkButton>
-      </GoldContainer>
+      </GoldContainer> */}
       <TheaterScreenings />
     </GreyContainer>
   );
