5d976a24623c21e0dbbe5a865f993fbd87b3ef49
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-10-24 14:10:32 +0300

    Store configured, index.js updated for redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './reset.css';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './custom.scss';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./reset.css";
+import "./App.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./custom.scss";
+import { BrowserRouter } from "react-router-dom";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
+
+reportWebVitals();
